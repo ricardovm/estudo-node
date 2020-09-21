a97c67e39f22565e33f2b47905c025098eb4971f
Ricardo Mannrich <mannrich@hotmail.com>
2020-09-20 21:14:37 -0300

    Persistindo dados

--- controllers/atendimento.js
+++ controllers/atendimento.js
@@ -1,8 +1,12 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos com GET'))
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body)
+        const atendimento = req.body
+
+        Atendimento.adicionar(atendimento)
         res.send('Você está na rota de atendimentos com POST')
     })
 }
